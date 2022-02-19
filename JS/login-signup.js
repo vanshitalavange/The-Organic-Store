a2218600bf024a2ffcb764166754a0694a26e70a
vanshitalavange <vanshita.a.lavange@gmail.com>
2022-02-19 15:51:34 +0530

    refactor: code format

--- JS/login-signup.js
+++ JS/login-signup.js
@@ -2,22 +2,28 @@ let loginSection = document.querySelector(".login");
 let signupSection = document.querySelector(".signup");
 let btnSignup = document.querySelectorAll(".btn-signup");
 let btnLogin = document.querySelectorAll(".btn-login");
-let btnCloseLoginSignupPopup = document.querySelector(".btn-close-login-signup-popup");
+let btnCloseLoginSignupPopup = document.querySelector(
+  ".btn-close-login-signup-popup"
+);
 let loginSignupSection = document.querySelector(".login-signup-section");
 let navBtnLogin = document.querySelector(".list-item-login");
 
-navBtnLogin.addEventListener("click",()=>{
-    loginSignupSection.style.display = "flex";
-})
+navBtnLogin.addEventListener("click", () => {
+  loginSignupSection.style.display = "flex";
+});
 
-btnLogin.forEach(login => login.addEventListener("click",()=>{
+btnLogin.forEach((login) =>
+  login.addEventListener("click", () => {
     loginSection.style.display = "flex";
-    signupSection.style.display = "none"
-}));
-btnSignup.forEach(signup => signup.addEventListener("click",()=>{
+    signupSection.style.display = "none";
+  })
+);
+btnSignup.forEach((signup) =>
+  signup.addEventListener("click", () => {
     signupSection.style.display = "flex";
     loginSection.style.display = "none";
-}));
-btnCloseLoginSignupPopup.addEventListener("click",()=>{
+  })
+);
+btnCloseLoginSignupPopup.addEventListener("click", () => {
   loginSignupSection.style.display = "none";
-})
+});
